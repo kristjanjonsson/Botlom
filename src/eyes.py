c1449c4b22f58d1287634ed91d745ccdf355076c
Kristjan Jonsson <kristjan.jonsson88@gmail.com>
2015-11-27 18:51:12 -0800

    Added topic with bounding boxes.

--- src/eyes.py
+++ src/eyes.py
@@ -16,12 +16,13 @@ class Eyes:
     '''The eyes of our beloved Botlom.'''
 
     def __init__(self, node_name):
+        self.detectors = [ColorDetector(*green), ColorDetector(*green)]
+        self.cap = cv2.VideoCapture(0)  # Does this work or do we need a ROS video nodes?
+
         rospy.init_node(node_name)
         self.bridge = CvBridge()
         self.raw = rospy.Publisher('raw', Image, queue_size=1)
-        self.cap = cv2.VideoCapture(0)  # Does this work or do we need a ROS video nodes?
-
-        self.detectors = ColorDetector(*green), ColorDetector(*green)
+        self.bounding_box = rospy.Publisher('bounding_box', Image, queue_size=1)
 
         rospy.Service('motion_mode_keyboard', ChangeColor, self.set_detector)
         rospy.loginfo('{0} initialized.'.format(node_name))
@@ -29,15 +30,27 @@ class Eyes:
     def watch(self):
         rate = rospy.Rate(24)
         while not rospy.is_shutdown():
-            success, img = self.cap.read()
+            success, frame = self.cap.read()
             if not success:
                 rospy.loginfo('Could not read image.')
                 break
 
-            img_msg = self.bridge.cv2_to_imgmsg(img, "bgr8")
+            # Publish the raw frame.
+            img_msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
             self.raw.publish(img_msg)
 
+            # Draw bounding boxes on frame and publish.
+            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            for i in range(2):
+                bbox = self.detectors[i].bounding_box(hsv_frame)
+                if bbox:
+                    (x, y, w, h) = bbox
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 0))
+            img_msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
+            self.bounding_box.publish(img_msg)
+
             rate.sleep()
+        rospy.loginfo('Shutting down.')
 
     def set_detector(self, request):
         try:
